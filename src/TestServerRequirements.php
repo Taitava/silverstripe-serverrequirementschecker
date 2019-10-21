58c4e67b1395767a6907c8b5afbcd0fd3b29f43b
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2019-10-22 00:23:46 +0300

    Fix TestServerRequirements.

--- src/TestServerRequirements.php
+++ src/TestServerRequirements.php
@@ -8,7 +8,20 @@ class TestServerRequirements extends SapphireTest
 {
 	public function testRequirements()
 	{
-		$checker = new ServerRequirementsChecker;
-		$this->assertEmpty($checker->getErrors(), 'Server environment errors detected. To pass this test, the list of errors should be empty.');
+		// Modify $_SERVER to make sure our test can pass
+		// Variables SCRIPT_NAME, HTTP_HOST and SCRIPT_FILENAME are checked by InstallRequirements, but they are not
+		// present when running unit tests. Populate them with some dummy values to make our way through these tests.
+		$backup_SERVER = $_SERVER;
+		$_SERVER['SCRIPT_NAME'] = __FILE__;
+		$_SERVER['HTTP_HOST'] = 'localhost';
+		$_SERVER['SCRIPT_FILENAME'] = __FILE__;
+		
+		$checker = new ServerRequirementsChecker(false); // TODO: Make it possible to configure to enable database checks during unit testing. Now it's just assumed that we do not have a db connection while doing unit tests.
+		$errors = $checker->getErrors();
+		$errors_string = print_r($errors, true);
+		$this->assertEmpty($errors, "Server environment errors detected. To pass this test, the list of errors should be empty. Errors:\n$errors_string");
+		
+		// Undo changes to $_SERVER
+		$_SERVER = $backup_SERVER;
 	}
 }
